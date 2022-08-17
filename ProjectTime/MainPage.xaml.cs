daa6e6adc487cf9895e8a70e8204e69eb1fe2da4
ShakedKod <82947988+Shakedkod@users.noreply.github.com>
2022-08-17 11:06:42 +0300

    lots of changes

--- ProjectTime/MainPage.xaml.cs
+++ ProjectTime/MainPage.xaml.cs
@@ -2,25 +2,9 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
-        }
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            Core.LogTest($"this is count: {count}");
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
         }
     }
 }
