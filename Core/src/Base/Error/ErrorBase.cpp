0771510e3e4cf9f3c2f8b2837b948ce697923225
ShakedKod <82947988+Shakedkod@users.noreply.github.com>
2022-08-21 10:38:42 +0300

    updated ProjectTime repository

--- Core/src/Base/Error/ErrorBase.cpp
+++ Core/src/Base/Error/ErrorBase.cpp
@@ -1,11 +1,44 @@
 #include "ErrorBase.h"
 
+#include <format>
+
 namespace Core
 {
     namespace Error
     {
-        Base::Base()
+        Base::Base(std::string Error, std::string Description, std::string FullDescription = "", std::string File, std::string Function, std::string ErrorLogFilePath = "Log.log")
         {
+            
+        }
+
+        Base::~Base()
+        {
+        }
+
+        std::string Base::ToString()
+        {
+            return std::format(
+                m_Message,
+                Error,
+                FilePath,
+                Line,
+                Function,
+                Description,
+                ErrorLogFilePath
+            );
+        }
+
+        std::string Base::FullString()
+        {
+            return std::format(
+                m_Message,
+                Error,
+                FullDescription,
+                FilePath,
+                Line,
+                Function,
+                ErrorLogFilePath
+            );
         }
     }
 }
--- Core/src/Base/Error/ErrorBase.h
+++ Core/src/Base/Error/ErrorBase.h
@@ -1,13 +1,47 @@
 #pragma once
 
+#include "src/Base/FileManagment/Basic_File.h"
+
+#include <string>
+
 namespace Core
 {
     namespace Error
     {
         class Base
         {
-            Base();
+            // (Con/De)structors
+            Base(
+                std::string Error,
+                std::string Description,
+                std::string FullDescription = "",
+                std::string File,
+                std::string Function,
+                std::string ErrorLogFilePath = "Log.log"
+            );
             ~Base();
+
+        public:
+            std::string ToString();
+            std::string FullString();
+
+        private:
+            std::string m_Message = "a {} has ocurred:\n\tfile : {}\n\tline : {}\n\tfunc : {}\n\treason : {}\nfor full info go to : {}";
+            std::string m_FullMessage = "a {} has ocurred:\n\tERROR:\n\t\t{}\n\tWHERE:\n\t\tfile: {}\n\t\tline: {}\n\t\tfunc: {}\n\t\tlog file: {}";
+
+            File m_ErrorLogFile;
+
+        public:
+            std::string Error;
+            std::string Description;
+            std::string FullDescription;
+
+            std::string FilePath;
+            std::string Function;
+
+            std::string ErrorLogFilePath;
+
+            int Line;
         };
     }
 }
