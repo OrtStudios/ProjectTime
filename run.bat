d4f2224b17e8670b02faea28971c757eb1d9adbb
ShakedKod <82947988+Shakedkod@users.noreply.github.com>
2022-08-31 12:00:15 +0300

    updated ProjectTime repository

--- run.bat
+++ run.bat
@@ -1,3 +1,7 @@
-@echo off
+
 echo runing ProjectTime
-C:\Dev\ProjectTime\bin\Release\ProjectTime\Release\net6.0-windows10.0.19041.0\win10-x64\ProjectTime.exe
+
+cd bin\Release\ProjectTime\Release\net6.0-windows10.0.19041.0\win10-x64\
+ProjectTime.exe
+
+cd ..\..\..\..\..\..\
