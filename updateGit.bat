d40096e62254622f70c01284a9adf532ff576e1c
ShakedKod <82947988+Shakedkod@users.noreply.github.com>
2023-01-21 22:25:50 +0200

    updated ProjectTime repository

--- updateGit.bat
+++ updateGit.bat
@@ -3,13 +3,29 @@
 echo pulling from github
 git pull
 
+cd Core
+git pull
+cd ..
+
 echo adding changes
 git add .
+
+cd Core
+git add .
+cd ..
 
 echo commiting
 git commit -m "updated ProjectTime repository"
 
+cd Core
+git commit -m "updated PT Core repository"
+cd ..
+
 echo pushing to github
 git push
+
+cd Core
+git push
+cd ..
 
 echo finished commiting and pushing
